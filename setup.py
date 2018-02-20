f7bd2c69d541344f98ad20cfeab7342af5fefe13
Maxim Semenov <0rang3max@gmail.com>
2018-02-21 00:47:26 +0300

    Wohoo, just deployed to pypi!

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
-    version='0.1',  # Required
+    version='0.2',  # Required
 
     # This is a one-line description or tagline of what your project does.
     description='Utility for translating numbers into Azerbaijani',  # Required
