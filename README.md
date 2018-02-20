079fe9b0fbcd709ab346b651f048d6df35898441
Maxim Semenov <0rang3max@gmail.com>
2018-02-20 18:09:49 +0300

    updated setup.py, ready for PyPi deploy

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # int2az
-
-TEST REPO
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='int2az',
+  packages=['int2az'],
+  version='0.1',
+  description='Utility for translating numbers into Azerbaijani.',
+  author='Maxim Semenov',
+  author_email='0rang3max@gmail.com',
+  url='https://github.com/0rang3max/int2az',  # use the URL to the github repo
+  download_url='https://github.com/0rang3max/int2az/archive/0.1.tar.gz',
+  keywords=['az', 'int2str'],  # arbitrary keywords
+  classifiers=[],
+)
